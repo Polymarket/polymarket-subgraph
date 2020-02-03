b67c82eb07b62c519cd76ca0a31e976073a6479c
Alan Lu <alanlu1023@gmail.com>
2020-02-03 11:41:02 -0600

    Add real startBlock to mapping writer

--- migrations/6_write_subgraph_files.js
+++ migrations/6_write_subgraph_files.js
@@ -2,17 +2,22 @@ const fs = require('fs-extra');
 const mustache = require('mustache');
 
 module.exports = function(deployer, network) {
-  for(const contractName of ['FPMMDeterministicFactory', 'FixedProductMarketMaker']) {
-    const { abi } = fs.readJsonSync(`build/contracts/${contractName}.json`);
-    fs.outputJsonSync(`abis/${contractName}.json`, abi, { spaces: 2 });
-  }
-
-  const template = fs.readFileSync('subgraph.template.yaml').toString();
-  fs.writeFileSync('subgraph.yaml', mustache.render(template, {
-    network,
-    FPMMDeterministicFactory: {
-      address: artifacts.require('FPMMDeterministicFactory').address,
-      startBlock: 0,
+  deployer.then(async () => {
+    for(const contractName of ['FPMMDeterministicFactory', 'FixedProductMarketMaker']) {
+      const { abi } = fs.readJsonSync(`build/contracts/${contractName}.json`);
+      fs.outputJsonSync(`abis/${contractName}.json`, abi, { spaces: 2 });
     }
-  }));
+  
+    const template = fs.readFileSync('subgraph.template.yaml').toString();
+    const FPMMDeterministicFactory = artifacts.require('FPMMDeterministicFactory');
+    fs.writeFileSync('subgraph.yaml', mustache.render(template, {
+      network,
+      FPMMDeterministicFactory: {
+        address: FPMMDeterministicFactory.address,
+        startBlock: (
+          await web3.eth.getTransactionReceipt(FPMMDeterministicFactory.transactionHash)
+        ).blockNumber,
+      }
+    }));
+  })
 };
