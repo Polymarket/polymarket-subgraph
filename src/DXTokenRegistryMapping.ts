cb4aba829a7be2602ff506f09457a897fe153e27
Nestor Amesty <nestor09amesty@gmail.com>
2020-07-04 18:40:27 +0200

    Mappings for omen curation created

--- src/DXTokenRegistryMapping.ts
+++ src/DXTokenRegistryMapping.ts
@@ -1,7 +1,31 @@
-export function handleAddToken(listId: number, market: string) {
+import { AddToken, RemoveToken } from '../generated/DXTokenRegistry/DXTokenRegistry'
+import { TokenRegistry } from '../generated/schema'
 
+export function handleAddToken(event: AddToken): void {
+  const id = event.params.listId.toHex()
+
+  if(id == '4') {
+    let tcr = TokenRegistry.load(id)
+
+    if(tcr != null) {
+      tcr = new TokenRegistry(id)
+      tcr.markets = []
+    }
+
+    tcr.markets.push(event.params.token.toHex())
+    tcr.save()
+  }
 }
 
-export function handleRemoveToken(listId: number, market: string) {
+export function handleRemoveToken(event: RemoveToken): void {
+  const id = event.params.listId.toHex()
 
+  if(id == '4') {
+    let tcr = TokenRegistry.load(id)
+
+    if(tcr != null) {
+      tcr.markets = tcr.markets.filter(token => token != event.params.token.toHex())
+      tcr.save()
+    }
+  }
 }
