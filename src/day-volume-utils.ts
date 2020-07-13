42a5a2d10c50b97a04e3eac25ca6d1c2b7801cb1
Alan Lu <alanlu1023@gmail.com>
2020-07-13 17:19:14 -0500

    Fix loss of running daily volume in sorting field workaround due to lack of significant digits

--- src/day-volume-utils.ts
+++ src/day-volume-utils.ts
@@ -8,11 +8,14 @@ let twoPow256Bytes = new Uint8Array(33) as Bytes;
 twoPow256Bytes.fill(0);
 twoPow256Bytes[32] = 1;
 let twoPow256 = BigInt.fromUnsignedBytes(twoPow256Bytes);
+let scaledVolumeGranularity = BigInt.fromI32(1000000);
 
 export function joinDayAndVolume(day: BigInt, volume: BigInt): BigInt {
   return day.times(twoPow256).plus(volume);
 }
 
-export function joinDayAndScaledVolume(day: BigInt, scaledVolume: BigDecimal): BigDecimal {
-  return day.times(twoPow256).toBigDecimal().plus(scaledVolume);
+export function joinDayAndScaledVolume(day: BigInt, volume: BigInt, collateralScale: BigInt): BigInt {
+  return day.times(twoPow256).times(scaledVolumeGranularity).plus(
+    volume.times(scaledVolumeGranularity).div(collateralScale)
+  );
 }
