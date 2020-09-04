78cfd409b05e035d703d7c33895313415edae275
Tom French <tom@tomfren.ch>
2020-09-04 16:32:49 +0100

    fix: Boolean -> boolean type fix

--- src/utils/conditional-utils.ts
+++ src/utils/conditional-utils.ts
@@ -4,7 +4,7 @@ import { BigInt } from '@graphprotocol/graph-ts'
  * Checks whether the partition corresponds to a full set
  * see: https://github.com/gnosis/conditional-tokens-contracts/blob/master/contracts/ConditionalTokens.sol
  */
-export function partitionCheck(partition: BigInt[], outcomeSlotCount: i32): Boolean {
+export function partitionCheck(partition: BigInt[], outcomeSlotCount: i32): boolean {
   let freeIndexSet = (1 << outcomeSlotCount) - 1;
   for (let i = 0; i < partition.length; i++) {
     let indexSet = partition[i];
