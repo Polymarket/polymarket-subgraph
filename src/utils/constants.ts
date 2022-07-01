370a599fdd2c5d689a02505ff3a6a3662c6d2fc3
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2022-07-01 20:21:12 -0300

    Supporting multiple events by transaction
    Fixes:
      - Supporting more than 1 event for transaction
      - The event key is a combination of txHash and orderHash to support all the scenarios
      - Removing unused variables and code
      - Fixing warnings

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 import { BigInt } from '@graphprotocol/graph-ts';
 
-export const bigZero = BigInt.fromI32(0);
-export const bigOne = BigInt.fromI32(1);
+export let bigZero = BigInt.fromI32(0);
+export let bigOne = BigInt.fromI32(1);
+
 export const AddressZero = '0x0000000000000000000000000000000000000000';
 
 export const TRADE_TYPE_BUY = "Buy"
