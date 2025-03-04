06f819fd879d44732b85be75a7adea2fe8cf9c02
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-03-03 22:01:08 -0500

    fix: sports oracle subgraph init

---
+++ sports-oracle-subgraph/README.md
@@ -0,0 +1,3 @@
+### sports-oracle-subgraph
+
+Subgraph tracking Games and Markets created with the UmaSportsOracle contract
---
+++ sports-oracle-subgraph/src/UmaSportsOracleMapping.ts
@@ -0,0 +1,47 @@
+
+
+export function handleGameCreated(event: GameCreated): void {
+
+    // TODO
+}
+
+export function handleGameSettled(event: GameSettled): void {
+
+    // TODO
+}
+
+export function handleGameEmergencySettled(event: GameEmergencySettled): void {
+
+    // 
+}
+
+export function handleGameCanceled(event: GameCanceled): void {
+
+    // TODO
+}
+
+export function handleGamePaused(event: GamePaused): void {
+    // 
+}
+
+export function handleGameUnpaused(event: GameUnpaused): void {
+
+    // TODO
+}
+
+export function handleMarketCreated(event: MarketCreated): void {
+
+    // TODO
+}
+
+export function handleMarketResolved(event: MarketResolved): void {
+
+    // TODO
+}
+
+
+export function handleMarketEmergencyResolved(event: MarketEmergencyResolved): void {
+    // TODO
+
+}
+
