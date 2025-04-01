020f9a3468e563251f0fd7f1531f0292d1281f68
best-journey <zxcvwert3458@gmail.com>
2025-04-01 23:18:16 +0100

    feat: store price

--- pnl-subgraph/src/ExchangeMapping.ts
+++ pnl-subgraph/src/ExchangeMapping.ts
@@ -1,10 +1,7 @@
 import { OrderFilled } from './types/Exchange/Exchange';
 
+import { Price } from './types/schema';
 import { parseOrderFilled } from './utils/parseOrderFilled';
-import { updateUserPositionWithBuy } from './utils/updateUserPositionWithBuy';
-import { updateUserPositionWithSell } from './utils/updateUserPositionWithSell';
-
-import { COLLATERAL_SCALE, TradeType } from '../../common/constants';
 
 /**
  * Handles individual OrderFilled events
@@ -23,22 +20,12 @@ event OrderFilled(
 export function handleOrderFilled(event: OrderFilled): void {
   const order = parseOrderFilled(event);
 
-  // dollars per share
-  const price = order.quoteAmount.times(COLLATERAL_SCALE).div(order.baseAmount);
+  let price = Price.load(order.positionId.toString());
 
-  if (order.side == TradeType.BUY) {
-    updateUserPositionWithBuy(
-      order.account,
-      order.positionId,
-      price,
-      order.baseAmount,
-    );
-  } else {
-    updateUserPositionWithSell(
-      order.account,
-      order.positionId,
-      price,
-      order.baseAmount,
-    );
+  if (price == null) {
+    price = new Price(order.positionId.toString());
   }
+
+  price.p = order.quoteAmount.div(order.baseAmount).toBigDecimal();
+  price.save();
 }
