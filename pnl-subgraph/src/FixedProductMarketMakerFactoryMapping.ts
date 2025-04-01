bd796f4df7ffc7d12b7c4c74510db1b77f3114ca
best-journey <zxcvwert3458@gmail.com>
2025-04-01 23:18:42 +0100

    feat: store fpmm which collateral token is usdc

--- pnl-subgraph/src/FixedProductMarketMakerFactoryMapping.ts
+++ pnl-subgraph/src/FixedProductMarketMakerFactoryMapping.ts
@@ -1,13 +1,13 @@
+import { USDC } from '../../common/constants';
 import { FixedProductMarketMakerCreation } from './types/FixedProductMarketMakerFactory/FixedProductMarketMakerFactory';
 import { FPMM } from './types/schema';
-import { FixedProductMarketMaker as FixedProductMarketMakerTemplate } from './types/templates';
 
 export function handleFixedProductMarketMakerCreation(
   event: FixedProductMarketMakerCreation,
 ): void {
-  const fpmm = new FPMM(event.params.fixedProductMarketMaker.toHexString());
-  fpmm.conditionId = event.params.conditionIds[0].toHexString();
-  fpmm.save();
-
-  FixedProductMarketMakerTemplate.create(event.params.fixedProductMarketMaker);
+  if (event.params.collateralToken == USDC) {
+    const fpmm = new FPMM(event.params.fixedProductMarketMaker.toHexString());
+    fpmm.conditionId = event.params.conditionIds[0].toHexString();
+    fpmm.save();
+  }
 }
