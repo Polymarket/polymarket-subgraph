7e7a9a0070ba9de8b514c34a60a6fb574693dce5
best-journey <zxcvwert3458@gmail.com>
2025-04-01 23:18:01 +0100

    feat: store historical pnl

--- pnl-subgraph/src/ConditionalTokensMapping.ts
+++ pnl-subgraph/src/ConditionalTokensMapping.ts
@@ -1,172 +1,68 @@
-import { BigInt, log } from '@graphprotocol/graph-ts';
-
 import {
-  ConditionPreparation,
-  ConditionResolution,
-  PositionSplit,
-  PositionsMerge,
-  PayoutRedemption,
+  TransferBatch,
+  TransferSingle,
 } from './types/ConditionalTokens/ConditionalTokens';
+import { createRealizedPnl } from './utils/createRealizedPnl';
 
-import { updateUserPositionWithSell } from './utils/updateUserPositionWithSell';
-import { updateUserPositionWithBuy } from './utils/updateUserPositionWithBuy';
-import { loadOrCreateUserPosition } from './utils/loadOrCreateUserPosition';
-
-import {
-  COLLATERAL_SCALE,
-  EXCHANGE,
-  FIFTY_CENTS,
-  NEG_RISK_ADAPTER,
-} from '../../common/constants';
-import { createCondition } from './utils/createCondition';
-import { loadCondition } from './utils/loadCondition';
-
-// SPLIT
-export function handlePositionSplit(event: PositionSplit): void {
-  const conditionId = event.params.conditionId;
-  const condition = loadCondition(conditionId);
-  if (condition == null) {
-    // ignore
-    return;
-  }
-
-  // - don't track splits from the NegRiskAdapter
-  //   these are handled in the NegRiskAdapterMapping
-  // - don't track splits from the CTFExchange
-  if (
-    [NEG_RISK_ADAPTER.toHexString(), EXCHANGE.toHexString()].includes(
-      event.params.stakeholder.toHexString(),
-    )
-  ) {
-    return;
-  }
-
-  // @ts-expect-error Cannot find name 'u8'.
-  let outcomeIndex: u8 = 0;
-  for (; outcomeIndex < 2; outcomeIndex++) {
-    const positionId = condition.positionIds[outcomeIndex];
-
-    updateUserPositionWithBuy(
-      event.params.stakeholder,
-      positionId,
-      FIFTY_CENTS,
-      event.params.amount,
-    );
-  }
-}
-
-// MERGE
-export function handlePositionsMerge(event: PositionsMerge): void {
-  const conditionId = event.params.conditionId;
-  const condition = loadCondition(conditionId);
-  if (condition == null) {
-    // ignore
-    return;
-  }
-
-  // - don't track merges from the NegRiskAdapter
-  //   these are handled in the NegRiskAdapterMapping
-  // - don't track merges from the CTFExchange
-  if (
-    [NEG_RISK_ADAPTER.toHexString(), EXCHANGE.toHexString()].includes(
-      event.params.stakeholder.toHexString(),
-    )
-  ) {
-    return;
-  }
-
-  // @ts-expect-error Cannot find name 'u8'.
-  let outcomeIndex: u8 = 0;
-  for (; outcomeIndex < 2; outcomeIndex++) {
-    const positionId = condition.positionIds[outcomeIndex];
-
-    updateUserPositionWithSell(
-      event.params.stakeholder,
-      positionId,
-      FIFTY_CENTS,
-      event.params.amount,
-    );
-  }
-}
-
-// REDEEM
-export function handlePayoutRedemption(event: PayoutRedemption): void {
-  const conditionId = event.params.conditionId;
-  const condition = loadCondition(conditionId);
-  if (condition == null) {
-    // ignore
-    return;
-  }
-
-  // - don't track redemptions from the NegRiskAdapter
-  //   these are handled in the NegRiskAdapterMapping
-  if (
-    [NEG_RISK_ADAPTER.toHexString()].includes(
-      event.params.redeemer.toHexString(),
-    )
-  ) {
-    return;
-  }
-
-  if (condition.payoutDenominator == BigInt.zero()) {
-    log.error('Failed to update market positions: payoutDenominator is 0', []);
-    return;
-  }
-
-  const payoutNumerators = condition.payoutNumerators;
-  const payoutDenominator = condition.payoutDenominator;
-
-  // @ts-expect-error Cannot find name 'u8'.
-  let outcomeIndex: u8 = 0;
-  for (; outcomeIndex < 2; outcomeIndex++) {
-    const positionId = condition.positionIds[outcomeIndex];
-
-    const userPosition = loadOrCreateUserPosition(
-      event.params.redeemer,
-      positionId,
-    );
-
-    // the user redeems their entire amount
-    const amount = userPosition.amount;
-    const price = payoutNumerators[outcomeIndex]
-      .times(COLLATERAL_SCALE)
-      .div(payoutDenominator);
-    updateUserPositionWithSell(
-      event.params.redeemer,
-      positionId,
-      price,
-      amount,
-    );
-  }
-}
-
-export function handleConditionPreparation(event: ConditionPreparation): void {
-  // we don't handle conditions with more than 2 outcomes
-  if (event.params.outcomeSlotCount.toI32() != 2) {
-    return;
-  }
-
-  const negRisk =
-    event.params.oracle.toHexString() == NEG_RISK_ADAPTER.toHexString();
-
-  // this is the only place we make new conditions !
-  const condition = createCondition(event.params.conditionId, negRisk);
-  condition.save();
-}
-
-export function handleConditionResolution(event: ConditionResolution): void {
-  const conditionId = event.params.conditionId;
-  const condition = loadCondition(conditionId);
-  if (condition == null) {
-    // ignore
-    return;
-  }
-
-  condition.payoutNumerators = event.params.payoutNumerators;
-  condition.payoutDenominator = event.params.payoutNumerators.reduce(
-    (cur, acc) => cur.plus(acc),
-    BigInt.zero(),
+/**
+ * Handles individual OrderFilled events
+event TransferSingle(
+  address indexed operator,
+  address indexed from,
+  address indexed to,
+  uint256 id,
+  uint256 value
+);
+ * @param event
+ */
+export function handleTransferSingle(event: TransferSingle): void {
+  createRealizedPnl(
+    event.block.timestamp,
+    event.transaction.hash,
+    event.params.from,
+    event.params.id,
+    event.params.value.neg(),
   );
+  createRealizedPnl(
+    event.block.timestamp,
+    event.transaction.hash,
+    event.params.to,
+    event.params.id,
+    event.params.value,
+  );
+}
 
-  condition.save();
+/**
+ * Handles individual OrderFilled events
+event TransferBatch(
+  address indexed operator,
+  address indexed from,
+  address indexed to,
+  uint256[] ids,
+  uint256[] values
+);
+ * @param event
+ */
+export function handleTransferBatch(event: TransferBatch): void {
+  const ids = event.params.ids;
+  const values = event.params.values;
+  const from = event.params.from;
+  const to = event.params.to;
+
+  for (let i = 0; i < ids.length; i++) {
+    createRealizedPnl(
+      event.block.timestamp,
+      event.transaction.hash,
+      from,
+      ids[i],
+      values[i].neg(),
+    );
+    createRealizedPnl(
+      event.block.timestamp,
+      event.transaction.hash,
+      to,
+      ids[i],
+      values[i],
+    );
+  }
 }
